8d39a767d4564e828c7d82c21b79861fbdcc055c
slavafleer <slavafleer@gmail.com>
2016-02-18 08:51:08 +0200

    ok

--- Tests/GSON/app/src/main/java/com/slavafleer/gson/MainActivity.java
+++ Tests/GSON/app/src/main/java/com/slavafleer/gson/MainActivity.java
@@ -2,17 +2,32 @@ package com.slavafleer.gson;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-public class MainActivity extends AppCompatActivity {
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity implements QuerySenderAsyncTask.Callbacks {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private static final int REQUEST_POIS = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        try {
+            URL url = new URL("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=31.2589,34.7997&type=cafe&rankby=distance&key=AIzaSyBqZywUvsonHXo6gVpiI0p-ABQ9oRuYdJw");
+            new QuerySenderAsyncTask(this, REQUEST_POIS).execute(url);
+
+        } catch (MalformedURLException e) {
+            Log.e(TAG, e.getMessage());
+        }
 
 
         Gson gson = new GsonBuilder().create();
@@ -39,5 +54,20 @@ public class MainActivity extends AppCompatActivity {
 //        JsonTest jsonTest = gson.fromJson(jsonResult, JsonTest.class);
 //
 //        System.out.println(jsonTest.toString());
+    }
+
+    @Override
+    public void onAboutToStartQuerySender() {
+
+    }
+
+    @Override
+    public void onSuccessQuerySender(String result, int respondId) {
+
+    }
+
+    @Override
+    public void onErrorQuerySender(String errorMessage, int respondId) {
+
     }
 }
---
+++ Tests/GSON/app/src/main/java/com/slavafleer/gson/QuerySenderAsyncTask.java
@@ -0,0 +1,104 @@
+package com.slavafleer.gson;
+
+import android.os.AsyncTask;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * AsyncTask for pois search request from Google Place API.
+ */
+public class QuerySenderAsyncTask extends AsyncTask<URL, Void, String> {
+
+    private Callbacks callbacks;
+    private int requestId;
+    private String errorMessage;
+
+    public QuerySenderAsyncTask(Callbacks callbacks, int requestKey) {
+        this.callbacks = callbacks;
+        this.requestId = requestKey;
+    }
+
+    // Precede before asyncTask.
+    @Override
+    protected void onPreExecute() {
+
+        callbacks.onAboutToStartQuerySender();
+    }
+
+    // Send query for results from api - done in parallel thread.
+    @Override
+    protected String doInBackground(URL... params) {
+
+        InputStream inputStream = null;
+        InputStreamReader inputStreamReader = null;
+        BufferedReader bufferedReader = null;
+
+        try {
+
+            URL url = params[0];
+
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+            int httpStatusCode = connection.getResponseCode();
+
+            if (httpStatusCode != HttpURLConnection.HTTP_OK) {
+                errorMessage = connection.getResponseMessage();
+                return null;
+            }
+
+            inputStream = connection.getInputStream();
+            inputStreamReader = new InputStreamReader(inputStream);
+            bufferedReader = new BufferedReader(inputStreamReader);
+
+            String result = "";
+
+            String oneLine = bufferedReader.readLine();
+
+            while (oneLine != null) {
+                result += oneLine + "\n";
+                oneLine = bufferedReader.readLine();
+            }
+
+            return result;
+
+        } catch (Exception ex) {
+            errorMessage = ex.getMessage();
+            return null;
+
+        } finally {
+            try {
+                bufferedReader.close();
+                inputStreamReader.close();
+                inputStream.close();
+
+            } catch (Exception e) {
+            }
+        }
+    }
+
+    // When result received, send it to main thread.
+    @Override
+    protected void onPostExecute(String result) {
+
+        if(errorMessage != null) {
+            callbacks.onErrorQuerySender(errorMessage, requestId);
+        }
+        else {
+            callbacks.onSuccessQuerySender(result, requestId);
+        }
+    }
+
+    // Interface for adding relevant actions in parent Class.
+    public interface Callbacks {
+
+        void onAboutToStartQuerySender();
+
+        void onSuccessQuerySender(String result, int respondId);
+
+        void onErrorQuerySender(String errorMessage, int respondId);
+    }
+}
